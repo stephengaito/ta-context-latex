0fdc8f5fc67f240c7a6b398dee8333001a5da5f4
Stephen Gaito <stephen@perceptisys.co.uk>
2017-03-08 11:19:25 +0000

    added contextMkIV 'langauge' module which knows how to produce the documents associated with a ConTeXt MkIV module

---
+++ contextMkIV/init.lua
@@ -0,0 +1,39 @@
+-- Copyright 2017 Stephen Gaito. See License.md
+
+local M = { }
+
+local function initContextMkIV(lexerName)
+  if lexerName == 'contextMkIV' then
+    -- Initialization for the contextMkIV module
+
+    -- add in ContextMkIV specific key codes
+    keys['contextMkIV'] = keys.context or {}
+    -- keys.context.cg = require('context/ctags').goto_symbol    -- Ctrl-g
+    --keys.context[not OSX and (GUI and 'cR' or 'cmr') or 'mR'] = require('common/clearMBuffer').clearMessageBufferRunCompile
+    keys.contextMkIV['cR'] = require('common/clearMBuffer').clearMessageBufferRunCompile
+    
+    -- add the mapping from the contextMkIV lexer to context
+    textadept.run.compile_commands['contextMkIV'] = 'mtxrun --script modules --process %f'
+    textadept.run.run_commands['contextMkIV']     = WIN32 and 'start "" "%e-mkiv.pdf"' or OSX and 'open "%e-mkiv.pdf"' or 'xdg-open "%e-mkiv.pdf"'
+    
+    -- add some latex snippets
+    snippets['contextMkIV']         = snippets['contextMkIV'] or {}
+    snippets.contextMkIV['start']   = 'start%1\n%0\n\\stop%1'
+    snippets.contextMkIV['itemize'] = 'startitemize\n\t%0\n\\stopitemize'
+    snippets.contextMkIV['enum']    = 'startenumerate\n\t%0\n\\stopenumerate'
+    snippets.contextMkIV['emph']    = 'emph{%0}'
+    snippets.contextMkIV['bold']    = 'textbf{%0}'
+    snippets.contextMkIV['doc']     = '????'
+    snippets.contextMkIV['chap']    = 'startchapter[title={%1}]\n%0\n\\stopchapter'
+    snippets.contextMkIV['sec']     = 'startsection[title={%1}]\n%0\n\\stopsection'
+    snippets.contextMkIV['ssec']    = 'startsubsection[title={%1}]\n%0\n\\stopsubsection'
+    snippets.contextMkIV['toc']     = 'tableofcontents'
+
+    -- add comment string for context
+    textadept.editing.comment_string['contextMkIV'] = '%'
+  end
+end
+
+events.connect(events.LEXER_LOADED, initContextMkIV)
+
+return M
